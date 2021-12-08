3b7cf18af7e0f89c94d10eb953ca018a150a2f55
Demarcus Lester <demarcuslester12@gmail.com>
2021-12-08 08:51:36 -0600

    Had to restart my entire project last night, am building the models for the post

--- django_wiki/models.py
+++ django_wiki/models.py
@@ -1,3 +1,14 @@
-from django.db import models
 
 # Create your models here.
+from django.contrib.auth import get_user_model
+from django.db import models
+
+class Wiki(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField(default="", max_length=100)
+    poster = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        #  this can be used to to return different things to be used 
+        return(f'{self.title} , {self.poster} ')
